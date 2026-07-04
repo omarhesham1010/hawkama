89db57d59ddaafb82799e1c77f8718272568f83d
Omar Hesham <omarhesham1192019@gmail.com>
2026-07-04 21:55:24 +0300

    Use exact PowerPoint template background

--- src/components/player/SlideTemplateFrame.tsx
+++ src/components/player/SlideTemplateFrame.tsx
@@ -2,34 +2,11 @@ export function SlideTemplateFrame() {
   return (
     <div className="pointer-events-none absolute inset-0 z-0 overflow-hidden bg-white" aria-hidden="true">
       <img
-        src="/template-assets/vision-watermark.png"
+        src="/template-assets/program-template-bg.png"
         alt=""
-        className="absolute -left-[18px] -top-[4px] h-[172px] w-[330px] object-contain opacity-[0.11]"
+        className="h-full w-full object-cover"
         draggable={false}
       />
-
-      <img
-        src="/template-assets/moh-capability-ab3ad.png"
-        alt=""
-        className="absolute right-[330px] top-[12px] h-[86px] w-[240px] object-contain"
-        draggable={false}
-      />
-      <img
-        src="/template-assets/ministry-health.png"
-        alt=""
-        className="absolute right-[8px] top-[14px] h-[56px] w-[198px] object-contain"
-        draggable={false}
-      />
-
-      <div className="absolute bottom-[25px] left-[109px] right-[18px] h-[2px] bg-[#0D8C60]" />
-      <img
-        src="/template-assets/moh-logo-small.png"
-        alt=""
-        className="absolute bottom-[18px] left-[63px] h-[32px] w-[49px] object-contain"
-        draggable={false}
-      />
-      <div className="absolute bottom-[17px] left-[15px] h-[39px] w-[39px] rounded-full border border-[#0D8C60]/20" />
-      <div className="absolute bottom-[29px] left-[27px] h-[15px] w-[15px] rounded-full bg-[#0D8C60]/20" />
     </div>
   );
 }
